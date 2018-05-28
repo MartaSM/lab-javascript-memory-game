9cfe6b0c18b177656f77dc1c9750625e1adff07d
Marta Soler <marta@MacBook-Air-de-Barrabes.local>
2018-05-28 22:38:19 +0200

    Exercise finished

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,38 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var newCardsArr = [];
+    for (i=0; i<cardsArr.length; i++) {
+    var random = Math.floor(Math.random()*cardsArr.length);
+    if (cardsArr[random] === "") {
+      i--;
+      continue;
+    }
+    newCardsArr.push(cardsArr[random]);
+    cardsArr[random] = "";
+    }
+    return newCardsArr;
+    };
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
+    if(firstCard === secondCard){
+        this.pairsGuessed++;
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+if (this.pairsGuessed !== 12){
+    return false;
+} else {
+    return true;
+}
+};
